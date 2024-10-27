dbb0cdbfbb57f37088a9103139be3fcc6233d34a
saadkou <saadkou@sadkoubane.website>
2024-10-27 16:57:30 +0100

    cartState : Signal refactoring of updateQuantity

--- src/app/services/cart.state.ts
+++ src/app/services/cart.state.ts
@@ -5,42 +5,53 @@ import { map } from 'rxjs/operators';
 import {Product} from "../models/product.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartState {
-
   //Change to a signale of cart Items
   //Nb:  A signal must always have an intial value
   readonly cartItems = signal<CartItem[]>([]);
 
+  //Computed Signal of Total Items
+  getTotalItems = computed(() =>
+    this.cartItems().reduce((sum, item) => sum + item.quantity, 0)
+  );
 
+  // Computed Signal for cart total
 
-//Computed Signal of Total Items
-  getTotalItems = computed(() => this.cartItems().reduce((sum, item) => sum +  item.quantity, 0) ) ;
-
-// Computed Signal for cart total
-
-  readonly total = computed(() => this.cartItems().reduce((sum, item) => sum + item.price * item.quantity, 0) ) ;
+  readonly total = computed(() =>
+    this.cartItems().reduce((sum, item) => sum + item.price * item.quantity, 0)
+  );
 
   // Method to add item to cart
   addToCart(product: Product): void {
-    const existingItem = this.cartItems().find(item => item.id === product.id);
+    const existingItem = this.cartItems().find(
+      (item) => item.id === product.id
+    );
 
     if (existingItem) {
-     this.cartItems.update(currentItems =>
-      currentItems.map(item => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item ));
+      this.cartItems.update((currentItems) =>
+        currentItems.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
     } else {
-      this.cartItems.update(currentItems => [...currentItems, { ...product, quantity: 1 }]);
+      this.cartItems.update((currentItems) => [
+        ...currentItems,
+        { ...product, quantity: 1 },
+      ]);
     }
   }
 
   // Method to update item quantity
   updateQuantity(itemId: number, quantity: number): void {
-    // const currentItems = this.cartItems$.value;
-    // const updatedItems = currentItems.map(item =>
-    //   item.id === itemId ? { ...item, quantity } : item
-    // );
-    // this.cartItems$.next(updatedItems);
+    this.cartItems.update((currentItems) =>
+      currentItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
   }
 
   // Method to remove item from cart
