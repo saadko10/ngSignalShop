eb76a09b431c686f37a2ca1f33e3f5cf71ca2734
saadkou <saadkou@sadkoubane.website>
2024-10-27 16:55:27 +0100

    cartState : Signal refactoring of addToCart

--- src/app/services/cart.state.ts
+++ src/app/services/cart.state.ts
@@ -24,19 +24,14 @@ export class CartState {
 
   // Method to add item to cart
   addToCart(product: Product): void {
-    // const currentItems = this.cartItems$.value;
-    // const existingItem = currentItems.find(item => item.id === product.id);
+    const existingItem = this.cartItems().find(item => item.id === product.id);
 
-    // if (existingItem) {
-    //   const updatedItems = currentItems.map(item =>
-    //     item.id === product.id
-    //       ? { ...item, quantity: item.quantity + 1 }
-    //       : item
-    //   );
-    //   this.cartItems$.next(updatedItems);
-    // } else {
-    //   this.cartItems$.next([...currentItems, { ...product, quantity: 1 }]);
-    // }
+    if (existingItem) {
+     this.cartItems.update(currentItems =>
+      currentItems.map(item => item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item ));
+    } else {
+      this.cartItems.update(currentItems => [...currentItems, { ...product, quantity: 1 }]);
+    }
   }
 
   // Method to update item quantity
